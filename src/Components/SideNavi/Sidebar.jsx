f57dd6ac34f00794f2199a52b627619031915454
anjana96n <94234293+anjana96n@users.noreply.github.com>
2022-12-20 01:51:57 +0530

    Navigation bar adding

---
+++ src/Components/SideNavi/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './SideNav.css';
+
+export default props => {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Photographers
+      </a>
+      <a className="menu-item" href="/salads">
+        Videographers
+      </a>
+      <a className="menu-item" href="/pizzas">
+        Free Album
+      </a>
+      <a className="menu-item" href="/desserts">
+        Equipments
+      </a>
+      <a className="menu-item" href="/desserts">
+        Presets
+      </a>
+      <a className="menu-item" href="/desserts">
+        Hotels
+      </a>
+    </Menu>
+  );
+};
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './HomePage.css'
+import Sidebar from '../../Components/SideNavi/Sidebar';
 
 
 export const HomePage = ()=>{
     return(
         <div>
+            <Sidebar/>
             <h1>this is Home page</h1>
             
 
