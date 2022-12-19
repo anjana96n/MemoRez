af4a2ab76ffcb59c70f1a21d06794a20b13b3226
pradeep <dharmasenatap@gmail.com>
2022-12-20 02:54:35 +0530

    add input feilds to signup

--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -3,8 +3,35 @@ import './SignUpPage.css'
 
 export const SignUpPage = ()=>{
     return(
-        <div>
-            <h1>this is SignUp page</h1>
+        <div id='signUpFullContainer'>
+            <div id = "signUpLeftContainer">
+                <form action="" id = "signUpForm">
+                    <input className='signUpInput' type ="text" placeholder = "First Name" required id = "firstName" />
+                    <input className='signUpInput' type ="text" placeholder = "Second Name" id = "lastName"/>
+                    <input className='signUpInput' type ="email" placeholder = "Email" required id = "email" />
+                    <input className='signUpInput' type="tel"  placeholder = "Mobile" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" id="phone"/>
+                    <select className='signUpInput' name="cars" id="province" >
+                        <option value="" disabled selected>Province</option>
+                        <option value="bp">Baticaloa</option>
+                        <option value="ep">Eastern</option>
+                        <option value="np">Northern</option>
+                        <option value="nc">North Central</option>
+                        <option value="nw">North Western</option>
+                        <option value="sg">Sabaragamuwa</option>
+                        <option value="sp">Southern</option>
+                        <option value="up">Uva</option>
+                    </select>
+                    <input className='signUpInput' type ="text" placeholder = "Adderess First line" id = "adreesFirstLine"/>
+                    <input className='signUpInput' type ="text" placeholder = "Adderess Second line" id = "adreesSecondLine"/>
+                    <input className='signUpInput' type ="password" placeholder = "Password" id = "password"/>
+                    <input className='signUpInput' type ="password" placeholder = "Conform Password" id = "conformPassword"/>
+                    <input className='signUpInput' type="submit" value = "Submmit" id= "btnSubmit"/>
+                </form>
+            </div>
+            <div id = "signUpRightContainer">
+
+            </div>
+            
         </div>
     )
 }
