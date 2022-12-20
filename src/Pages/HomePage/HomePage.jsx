05d29cf06f2ee6ab47662793d84d1c0fe917ec68
anjana96n <94234293+anjana96n@users.noreply.github.com>
2022-12-20 22:34:08 +0530

    Home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,20 +2,27 @@ import React from 'react'
 import './HomePage.css'
 import Sidebar from '../../Components/SideNavi/Sidebar';
 
+
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Container from 'react-bootstrap/Container';
 
-
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const HomePage = ()=>{
     return(
+      
         <div>
-            <Sidebar/>
+          <div> <Sidebar/> </div>
+          <Container>
+      <Row>
+        <Col xs={{ order: 'last' }}>
             <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
@@ -35,10 +42,9 @@ export const HomePage = ()=>{
         <Button size="small">Details</Button>
         <Button size="small">Contact</Button>
       </CardActions>
-    </Card>
+    </Card></Col>
 
-
-    <Card sx={{ maxWidth: 345 }}>
+        <Col xs><Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
@@ -57,7 +63,174 @@ export const HomePage = ()=>{
         <Button size="small">Details</Button>
         <Button size="small">Contact</Button>
       </CardActions>
-    </Card>
+    </Card></Col>
+
+        <Col xs={{ order: 'first' }}><Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://www.spadestudio.lk/wp-content/uploads/2020/07/thejani-tharindu-wedding-highlights-610x448.jpg"
+        alt="Kandy"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kasun
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Kandy
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+      </Row>
+<Row></Row>
+<Row></Row>
+<Row></Row>
+      <Row>
+        <Col xs={{ order: 'last' }}>
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://banuphotography.com/wp-content/uploads/2021/06/mannar-wedding-77.jpg"
+        alt="Jaffna"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Deshan
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Jaffna
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+
+        <Col xs><Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://www.spadestudio.lk/wp-content/uploads/2020/07/thejani-tharindu-wedding-highlights-610x448.jpg"
+        alt="Kandy"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kasun
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Kandy
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+
+        <Col xs={{ order: 'first' }}><Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://www.spadestudio.lk/wp-content/uploads/2020/07/thejani-tharindu-wedding-highlights-610x448.jpg"
+        alt="Kandy"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kasun
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Kandy
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+      </Row>
+
+<Row></Row>
+<Row></Row>
+<Row></Row>
+
+<Row>
+        <Col xs={{ order: 'last' }}>
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://banuphotography.com/wp-content/uploads/2021/06/mannar-wedding-77.jpg"
+        alt="Jaffna"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Deshan
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Jaffna
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+
+        <Col xs><Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://www.spadestudio.lk/wp-content/uploads/2020/07/thejani-tharindu-wedding-highlights-610x448.jpg"
+        alt="Kandy"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kasun
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Kandy
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+
+        <Col xs={{ order: 'first' }}><Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://www.spadestudio.lk/wp-content/uploads/2020/07/thejani-tharindu-wedding-highlights-610x448.jpg"
+        alt="Kandy"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kasun
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          I will cover your wedding and preshoot around Kandy
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Details</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card></Col>
+      </Row>
+
+
+    </Container>
+           
+
+
+    
             
             
 
