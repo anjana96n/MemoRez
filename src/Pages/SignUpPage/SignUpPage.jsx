a8a7987fe8b55fc07304d7e25f28899e839d9b23
pradeep <dharmasenatap@gmail.com>
2022-12-20 22:48:03 +0530

    responsive design of signup page

--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import './SignUpPage.css'
 
 export const SignUpPage = ()=>{
+
+    const summitionValidation = () =>{
+        alert('success');
+    }
     return(
         <div id='signUpFullContainer'>
             <div id = "signUpLeftContainer">
+                <h2>Sign Up</h2>
                 <form action="" id = "signUpForm">
                     <input className='signUpInput' type ="text" placeholder = "First Name" required id = "firstName" />
                     <input className='signUpInput' type ="text" placeholder = "Second Name" id = "lastName"/>
                     <input className='signUpInput' type ="email" placeholder = "Email" required id = "email" />
-                    <input className='signUpInput' type="tel"  placeholder = "Mobile" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" id="phone"/>
+                    <input className='signUpInput' type="tel"  placeholder = "Mobile" name="phone" pattern="[0-9]{10}" id="phone"/>
                     <select className='signUpInput' name="cars" id="province" >
                         <option value="" disabled selected>Province</option>
                         <option value="bp">Baticaloa</option>
@@ -25,7 +30,7 @@ export const SignUpPage = ()=>{
                     <input className='signUpInput' type ="text" placeholder = "Adderess Second line" id = "adreesSecondLine"/>
                     <input className='signUpInput' type ="password" placeholder = "Password" id = "password"/>
                     <input className='signUpInput' type ="password" placeholder = "Conform Password" id = "conformPassword"/>
-                    <input className='signUpInput' type="submit" value = "Submmit" id= "btnSubmit"/>
+                    <input className='signUpInput' type="submit" value = "Submmit" onSubmit={summitionValidation} id= "btnSubmit"/>
                 </form>
             </div>
             <div id = "signUpRightContainer">
